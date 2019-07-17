36780826439aac198ef0884a3dbd741e5dcc0941
Jehdi Aizon <46204336+jaizon2000@users.noreply.github.com>
2019-07-17 12:08:20 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
 # Resources
+
 ## Pallete
 
 https://coolors.co/86879e-f3f0ec-495466-252627-32343d
 
 ## Images
+
 bg.png - https://is.gd/hqYi9k
 ~~girl.png | boy.png - https://bit.ly/30AORti~~
 map tiles - https://is.gd/kIxQgN
 knight.png - https://bit.ly/2Sko7Kx
 
 ## Tutorials Used
+
 * CSS Selectors - https://bit.ly/2SghGbi 
 * Remove scrollbars - https://bit.ly/2SkpZmx
 * Perfect Centering - https://bit.ly/2xRvoYQ
 * More Centering:
-	* http://www.tipue.com/blog/center-a-div/
-	* https://www.w3.org/Style/Examples/007/center.en.html
-	* https://bit.ly/30tuQoq
+    + http://www.tipue.com/blog/center-a-div/
+    + https://www.w3.org/Style/Examples/007/center.en.html
+    + https://bit.ly/30tuQoq
+* Position Property - https://www.w3schools.com/css/css_positioning.asp
+* HTML DOM events list - https://www.w3schools.com/jsref/dom_obj_event.asp
+
--- main.js
+++ main.js
@@ -15,12 +15,46 @@ else if (window.location.pathname == "/play.html") {
 
 // --- PLAY.HTML --- //
 function play() {
-    document.getElementById('reset-btn').addEventListener('click', reset);
+    // Event Listeners //
+    //--------------- //
 
+    // Reset Button
+    document.getElementById('reset-btn').addEventListener('click', reset);
+    // How To Button
+    document.getElementById('how-to-btn').addEventListener('click', showHowTo);
+    document.body.addEventListener('mousedown', hideHowTo);
+    // Name
+    document.addEventListener('keydown', keyDown);
+    
+
+    // Functions //
+    //---------- //
+    // space bar event.keyCode == 32
+
+    // 
+
+    // Reset
     function reset() {
         console.log('randomize grid and restart all stats');
     }
 
-    var name = document.getElementById('name').value;
-    console.log(name);
+    // Get Value On ENTER
+    function keyDown(event) {
+        let key = event.keyCode;
+
+        if (key == 13) { // ENTER Key
+            let name = document.getElementById('name').value;
+            console.log(name);
+        }
+    }
+
+    // HIDE & SHOW HOW TO
+    function showHowTo() {
+        document.getElementById('how-to').classList.remove('hide');
+    }
+    function hideHowTo() {
+        document.getElementById('how-to').classList.add('hide');
+        console.log('hiding');
+    }
+
 }
