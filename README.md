dfe28092230da4ce69374e4c3d510e31304f4282
Jehdi Aizon <46204336+jaizon2000@users.noreply.github.com>
2019-07-17 09:20:32 -0600

    style changes and main addition
    added functions to some buttons

--- README.md
+++ README.md
@@ -1,31 +1,19 @@
 # Resources
-
 ## Pallete
+
 https://coolors.co/86879e-f3f0ec-495466-252627-32343d
 
-------
 ## Images
-
 bg.png - https://is.gd/hqYi9k
-
 ~~girl.png | boy.png - https://bit.ly/30AORti~~
-
 map tiles - https://is.gd/kIxQgN
-
 knight.png - https://bit.ly/2Sko7Kx
 
-------
 ## Tutorials Used
-
-* https://bit.ly/30tuQoq
-
-* https://bit.ly/2JBUuSo
-
-* CSS Selectors - https://bit.ly/2SghGbi
-
+* CSS Selectors - https://bit.ly/2SghGbi 
 * Remove scrollbars - https://bit.ly/2SkpZmx
-
 * Perfect Centering - https://bit.ly/2xRvoYQ
 * More Centering:
-    * http://www.tipue.com/blog/center-a-div/
-    * https://www.w3.org/Style/Examples/007/center.en.html
+	* http://www.tipue.com/blog/center-a-div/
+	* https://www.w3.org/Style/Examples/007/center.en.html
+	* https://bit.ly/30tuQoq
--- main.js
+++ main.js
@@ -2,12 +2,25 @@
 'use strict';
 
 // Event Listener
-document.getElementById('enter').addEventListener('click', startPage);
 
-// Functions
-function startPage() {
+// --- INDEX.HTML --- //
+if (window.location.pathname == "/index.html") {
+    console.log('take me home, country road');
+}
+else if (window.location.pathname == "/play.html") {
+    console.log('play to win');
+    play()
+    
+}
+
+// --- PLAY.HTML --- //
+function play() {
+    document.getElementById('reset-btn').addEventListener('click', reset);
+
+    function reset() {
+        console.log('randomize grid and restart all stats');
+    }
+
     var name = document.getElementById('name').value;
     console.log(name);
-    document.getElementById('start-page').classList.add('hide');
-    document.getElementById('play-page').classList.remove('hide');
 }
